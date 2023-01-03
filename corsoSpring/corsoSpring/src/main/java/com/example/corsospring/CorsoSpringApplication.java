df54cd0a3c1b879479866a278b6ddb853a9bbd19
robertotelesio <telesioroberto@gmail.com>
2023-01-03 16:47:02 +0100

    Second Commit

---
+++ corsoSpring/corsoSpring/src/main/java/com/example/corsospring/CorsoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.corsospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorsoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorsoSpringApplication.class, args);
+    }
+
+}
